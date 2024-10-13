e4cd659c3337c3e88c963d625c91f02033328984
geraldkachi <fitzgeraldkachi@gmail.com>
2024-10-13 13:37:52 +0100

    map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import { urlFor } from "@/sanity/utils";
+import NigeriaSvg from "./NigeriaSvg";
 
 const Map = ({ homepage }) => {
   return (
@@ -9,15 +10,16 @@ const Map = ({ homepage }) => {
         <h2 className="text-center md:text-[52px] text-3xl font-zilla md:pb-14 pb-10">
           Servicing all 36 States
         </h2>
-        <div className="md:px-20 px-5 md:block hidden">
-          <Image
+        <div className="md:px-20 px-5 md:flex hidden w-full  items-center justify-center">
+          <NigeriaSvg />
+          {/* <Image
             src={urlFor(homepage?.map).url()}
             width={1222}
             height={880}
             alt="map"
-          />
+          /> */}
         </div>
-        <div className="md:px-20 px-5 md:hidden">
+        <div className="md:px-20 px-5 flex items-center justify-center md:hidden">
           <Image
             src={urlFor(homepage?.map).url()}
             width={322.52}
